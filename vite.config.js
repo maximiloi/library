5d5660761f40ff06fdb020416b737f3bf744d244
maximiloi <maximiloi@gmail.com>
2023-10-09 19:00:45 +0300

    fix: w3c errors, add alt img sec about, self-closing tag at pug

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import pugPlugin from 'vite-plugin-pug';
 
-const options = { pretty: true };
+const options = { doctype: 'html', pretty: true };
 const locals = { name: 'My Pug' };
 
 export default defineConfig({
